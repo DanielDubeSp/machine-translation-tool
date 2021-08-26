ad9c039975e4bed5d25bb2533376e0ad8638c1f6
DanielDubeSp <danidube@gmail.com>
2021-08-26 20:30:26 +0200

    Removed template files

---
+++ MachineTranslationTool.API/Controllers/TranslateController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MachineTranslationTool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslateController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        private readonly ILogger<TranslateController> _logger;
+
+        public TranslateController(ILogger<TranslateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult("{Hello world}");
+        }
+    }
+}
