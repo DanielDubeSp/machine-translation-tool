6eccaf09ba17365134240b30c749ac857f588c6e
DanielDubeSp <danidube@gmail.com>
2021-08-26 23:30:09 +0200

    Very basic translation API

--- MachineTranslationTool.API/Controllers/TranslateController.cs
+++ MachineTranslationTool.API/Controllers/TranslateController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace MachineTranslationTool.Controllers
 {
@@ -11,22 +10,37 @@ namespace MachineTranslationTool.Controllers
     [Route("[controller]")]
     public class TranslateController : ControllerBase
     {
-        //private static readonly string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
-
         private readonly ILogger<TranslateController> _logger;
+        private readonly string pyPath = @"C:\Users\DANIEL.DUBE\AppData\Local\Programs\Python\Launcher\py.exe";
 
         public TranslateController(ILogger<TranslateController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="langTgt">en,es,de</param>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string text, string langTgt)
         {
-            return new JsonResult("{Hello world}");
+            var testScript = Path.Combine(AppContext.BaseDirectory, "Translate.py");
+
+            ProcessStartInfo start = new()
+            {
+                FileName = pyPath,
+                Arguments = string.Format("{0} {1} {2}", testScript, "\"" + text + "\"", "\"" + langTgt + "\""),
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+            using Process process = Process.Start(start);
+            using StreamReader reader = process.StandardOutput;
+            string result = reader.ReadToEnd();
+            return new OkObjectResult(result);
+
         }
     }
 }
--- MachineTranslationTool.API/Translate.py
+++ MachineTranslationTool.API/Translate.py
@@ -2,7 +2,15 @@
 # pip3 install requests
 # pip3 install six
 
+import sys
+
 from google_trans_new import google_translator
 translator = google_translator()
-translate_text = translator.translate("Hola mundo!", lang_src="es", lang_tgt="en")
+#translate_text = translator.translate("Hola mundo!", lang_src="es", lang_tgt="en")
+#translate_text = translator.translate(sys.argv[1],lang_src=sys.argv[2],lang_tgt=sys.argv[3])
+translate_text = translator.translate(sys.argv[1],lang_tgt=sys.argv[2]) # API can automatically identify the src translation language
+#print(sys.argv[1])
+#print(sys.argv[2])
+#print(sys.argv[3])
 print(translate_text)
+sys.stdout.flush()
